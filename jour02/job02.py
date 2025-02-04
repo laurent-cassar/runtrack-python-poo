d7d72307f2142fde3188b77670c284761a932843
laurent-cassar <laurent.cassar@laplateforme.io>
2025-02-04 15:18:01 +0100

    Create class Livre with attributs privés titre, auteur & nombre de pages with getter & setter for to display and to set the attributs of class, and a condition to verify nombre de pages integer and superior at 0

---
+++ jour02/job02.py
@@ -0,0 +1,26 @@
+class Livre:
+    def __init__(self, titre, auteur, nb_pages):
+        self.__titre = titre
+        self.__auteur = auteur
+        self.__nb_pages = nb_pages
+
+    def get_titre(self):
+        return self.__titre
+
+    def set_titre(self, titre):
+        self.__titre = titre
+
+    def get_auteur(self):
+        return self.__auteur
+
+    def set_auteur(self, auteur):
+        self.__auteur = auteur
+
+    def get_nb_pages(self):
+        return self.__nb_pages
+
+    def set_nb_pages(self, nb_pages):
+        if isinstance(nb_pages, int) and nb_pages > 0:
+            self.__nb_pages = nb_pages
+        else:
+            print("Erreur : le nombre de pages doit être un entier positif.")
