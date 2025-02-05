6180069be978ed64474be283d29d5931bc1a0894
laurent-cassar <laurent.cassar@laplateforme.io>
2025-02-05 17:38:22 +0100

    Creation of class Task and List of Tasks

---
+++ jour03/job03.py
@@ -0,0 +1,24 @@
+class Tache:
+    def __init__(self, titre, description, statut="à faire"):
+        self.titre = titre
+        self.description = description
+        self.statut = statut
+
+class ListeDeTaches:
+    def __init__(self):
+        self.taches = []
+
+    def ajouterTache(self, tache):
+        self.taches.append(tache)
+
+    def supprimerTache(self, tache):
+        self.taches.remove(tache)
+
+    def marquerCommeFinie(self, tache):
+        tache.statut = "terminée"
+
+    def afficherListe(self):
+        return self.taches
+
+    def filterListe(self, statut):
+        return [tache for tache in self.taches if tache.statut == statut]
