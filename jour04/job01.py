9f568664ca23fb060567753ca7ee48b12acbe712
laurent-cassar <laurent.cassar@laplateforme.io>
2025-02-07 14:35:52 +0100

    Creation of class personne and class Eleve with legacy of class Personne

---
+++ jour04/job01.py
@@ -0,0 +1,31 @@
+class Personne:
+    def __init__(self, age=14):
+        self.age = age
+
+    def afficherAge(self):
+        print(f"J'ai {self.age} ans")
+
+    def bonjour():
+        print("Hello")
+
+    def modifierAge(self, nouvel_age):
+        self.age = nouvel_age
+
+class Eleve(Personne):
+    def allerEnCours(self):
+        print("Je vais en cours")
+    
+    def afficherAge(self):
+        print(f"J'ai {self.age} ans")
+
+class Professeur:
+    def __init__(self, matiereEnseignee):
+        self.__matiereEnseignee = matiereEnseignee
+
+    def enseigner(self):
+        print("Le cours va commencer")
+
+personne = Personne()
+
+eleve = Eleve()
+eleve.afficherAge()
